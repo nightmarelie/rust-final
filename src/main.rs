2593bc0caac18423c178127ca459985380cf3dbc
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-10-07 21:54:40 -0600

    feat: refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}};
+use std::time::Duration;
+use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,10 +15,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -28,4 +32,3 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write_all(response.as_bytes()).unwrap();
 }
-
