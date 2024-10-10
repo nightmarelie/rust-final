a5fd46cc9703ed5d0fd9f5deb0c39466ed7350da
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-10-10 13:33:38 -0600

    feat: create alias

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub struct ThreadPool {
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -37,7 +37,11 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
-    {}
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
 }
 
 struct Worker {
