6320f645b51110fd87568b5344cb1f677e1d8ffa
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-10-10 13:13:02 -0600

    feat: create pool

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use std::{fs, io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
