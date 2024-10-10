39b37cddb52130de2f92ef6b3eea99b1f93593bf
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-10-10 13:39:23 -0600

    feat: wrap in some

--- src/lib.rs
+++ src/lib.rs
@@ -49,14 +49,16 @@ impl Drop for ThreadPool {
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
-            worker.thread.join().unwrap();
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
         }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -69,7 +71,10 @@ impl Worker {
             job();
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
 
